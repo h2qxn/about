2a4a1f98a4b25e8d98f9cb038202f1160707418a
h2qxn <68368968+h2qxn@users.noreply.github.com>
2021-10-21 20:37:34 +0800

    Update operator.cpp

--- 操作符重载/operator.cpp
+++ 操作符重载/operator.cpp
@@ -1,5 +1,43 @@
-#include<string>
+#include <iostream>
+using namespace std;
 
-void main()
+class wallet 
 {
+	public:
+		wallet(int s, int p);
+	    ~wallet();
+		int get();        // 返回钱包中有多少钱
+		int add(int);    // 往钱包中塞钱
+		int minus(int);  // 取钱 
+	private:
+		int size;     //钱包的外观大小
+		int price;    //钱包的价格
+		int money;    //钱包中的现金 
+		
+};
+wallet::wallet(int s, int p):size(s),price(p)
+{
+}
+wallet::~wallet()
+{
+}
+int wallet::get()
+{
+	return money;
+}
+int wallet::add(int n)
+{
+	return 0;
+}
+int wallet::minus(int n)
+{
+	return 0;
+}
+
+int main()
+{
+	 wallet w(10,100);
+	 cout<<w.get()<<endl;
+   cout << "Hello World";
+   return 0;
 }
